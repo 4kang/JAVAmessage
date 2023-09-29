8c7756244ca655d8b43b2559f2774410f86ddb29
Kono <konotori08@gmail.com>
2023-09-29 21:32:28 +0900

    Hukushu Commit

--- src/main/java/schoo/controller/SchooController.java
+++ src/main/java/schoo/controller/SchooController.java
@@ -1,53 +1,36 @@
 package schoo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import jakarta.servlet.http.HttpSession;
-
-/**
- * Springで作成する簡単なセッション管理のサンプルクラスです
- */
-@Controller
+@Controller //このファイル(class)がControllerだと目印つける
 public class SchooController {
 	
-	@Autowired
-	private HttpSession session;
-	
-	/**
-	 * トップページのリクエスト
-	 * @return input.htmlのパス
-	 */
-	@GetMapping("/")
+	//アノテーション
+	@GetMapping("/") // http://localhost:8080/
+	//Stringの文字列を返却するメソッド
 	public String index() {
-		//input.htmlのパスを返却する
-		return "input";
-	}
-	/**
-	 * セッション処理リクエスト
-	 * @param message メッセージ
-	 * @return input.htmlのパス
-	 */
-	@PostMapping("input")
-	public String input(@RequestParam("message") String message) {
-		//　現在のセッション内の情報を表示
-		String sessionMessage = (String)session.getAttribute("sessionMessage");
-		System.out.println("----- メッセージ（追加前） -----");
-		System.out.println(sessionMessage);
-		
-		//　メッセージを追加してセッション内に格納
-		//　少し見やすくするために空白文字を間に入れる
-		sessionMessage = sessionMessage + " " + message;
-		session.setAttribute("sessionMessage", sessionMessage);
-		
-		System.out.println("----- メッセージ（追加後）-----");
-		System.out.println(sessionMessage);
-		
-		return "finish";
+		return "index"; //index文字列を返却
 	}
 	
+	@PostMapping("login") // http://localhost:8080/login
+	//loginのリクエストを受け付けるメソッド
+	public String login(@RequestParam("loginId")String loginId,
+			            @RequestParam("loginPassword")String loginPassword) {
+		//変数 = loginID,password
+		
+		//初期Id,初期Passwordを引数に設定
+		if("schoo".equals(loginId) && "pass".equals(loginPassword)) {
+			//ログイン成功
+			return "login-ok"; //htmlのファイル名を指定
+		}else {
+			//ログイン失敗
+			return "login-ng";
+			
+		}
+		
+	}
 
 }
